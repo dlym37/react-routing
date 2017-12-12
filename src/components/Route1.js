f75faeef4608ce00341508185d7344439fc3106a
Dano Lym <dlym37@gmail.com>
2017-12-12 11:45:03 -0700

    completed

---
+++ src/components/Route1.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Route1(){
+    return <h1>Route 1</h1>
+
+}
---
+++ src/components/Route2.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Route2(){
+    return <h1>Route 2</h1>
+}
